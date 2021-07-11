62c45fca1f5807b3e76af467b2bf6002ac25e57a
bayramkeles61 <bayramkeles61@gmail.com>
2021-07-11 19:37:10 +0300

    store -> index.js, modules->post.js

---
+++ store/modules/post.js
@@ -0,0 +1,24 @@
+const state = () => ({
+  posts: []
+})
+
+const getters = {
+
+}
+
+const actions = {
+
+}
+
+const mutations = {
+
+}
+
+const postModule = {
+  state,
+  getters,
+  mutations,
+  actions
+}
+
+export default postModule
